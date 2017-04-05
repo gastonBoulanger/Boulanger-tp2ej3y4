faa3f2ef606203b8c15fe59e090d007a660c91a9
Gaston Boulanger <Gaston Boulanger>
2017-04-05 19:29:34 -0300

    entrega tp 2 entregable

--- src/tp2ej3y4/Main.java
+++ src/tp2ej3y4/Main.java
@@ -4,52 +4,65 @@ import java.util.Date;
 
 public class Main {
 
-	final static int size = 100000;
+	final static int size = 50000;
+	final static int random = 1000000;
+	
 	public static void main(String[] args) {
+		int sizePrint = size/2;
 		// ------------------------ EJERCICIO 3 ----------------------
 		int[] arr1 = new int[size];
 		int[] arr2 = new int[size];
 		BubbleSort bs = new BubbleSort();
 
+		//Se cargan los arreglos
 		for (int i = 0; i < size; i++) {
-			int temp = (int) (Math.random() * 1000000);
+			int temp = (int) (Math.random() * random);
 			arr1[i] = temp;
 			arr2[i] = temp;
 		}
 
 		System.out.println();
 		System.out.println("ARREGLO ORDENADO BURBUJA");
-		Date inicioArray = new Date();
+		//Se ordena por burbuja
+		Date inicioBubble = new Date();
 		bs.burble(arr1);
-		Date finalArray = new Date();
+		Date finalBubble = new Date();
 
 		// ------------------------ EJERCICIO 4 ----------------------
+		//Se ordena por mergesort
 		MergeSort merge = new MergeSort();
 		Date inicioMerge = new Date();
 		merge.sort(arr2);
 		Date finalMerge = new Date();
 
+		//Se imprime inicio y fin de ordenamiento de burbuja
 		System.out.println();
-		System.out.println(inicioArray);
-		System.out.println(finalArray);
+		System.out.println(inicioBubble);
+		System.out.println(finalBubble);
 
-		long resultadoArray = finalArray.getTime() - inicioArray.getTime();
-		System.out.println(resultadoArray / 1000 + " segundos con BUBBLESORT");
+		//Se calcula e imprime el tiempo final de ordenamiento burbuja en long
+		long resultBubble = finalBubble.getTime() - inicioBubble.getTime();
+		System.out.println(resultBubble + " con BUBBLESORT");
 
+		//Se imprime inicio y fin de ordenamiento por merge
 		System.out.println();
 		System.out.println(inicioMerge);
 		System.out.println(finalMerge);
 
-		long resultadoMerge = finalMerge.getTime() - inicioMerge.getTime();
-		System.out.println(resultadoMerge / 1000 + " segundos con MERGESORT");
+		//Se calcula e imprime el tiempo final de ordenamiento merge en long
+		long resultMerge = finalMerge.getTime() - inicioMerge.getTime();
+		System.out.println(resultMerge  + " con MERGESORT");
 
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < sizePrint; i++) {
+			System.out.print(arr1[i] + " - ");
+		}
+		System.out.println(" - ");
+		for (int i = 0; i < sizePrint; i++) {
 			System.out.print(arr2[i] + " - ");
 		}
-		/*Se puede observar que el tiempo del metodo burbujeo 
-		 *es ampliamente mayor que el tiempo empleado por el metodo mergesort
-		 *(mergesort practicamente tarda 0 segundos)
-		 *para ordenar el mismo arreglo.
+		/*En arreglo de tamaño chico se puede observar que el tiempo del metodo burbujeo
+		 *es menor que el tiempo empleado por el metodo mergesort
+		 *Por el contrario cuanto mas grande es el arreglo mas rapido es el mergesort.
 		 */
 	}
 }
